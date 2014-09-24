37edd0d786c8c9043c151235d0a646c246c70c31
derek <github@derekgillett.com>
2014-09-25 08:05:00 +1000

    minor changes - remove comments

--- ClasherCalendar/src/com/derekgillett/clashercalendar/MainActivity.java
+++ ClasherCalendar/src/com/derekgillett/clashercalendar/MainActivity.java
@@ -234,16 +234,10 @@ public class MainActivity extends ActionBarActivity {
 	// show a view of the element list
 	private void showElementView() {
         Fragment fragment = new ElementFragment();
-    //    Bundle args = new Bundle();
-//        args.putInt( Element PlanetFragment.ARG_PLANET_NUMBER, position);
-  //      fragment.setArguments(args);
 
         FragmentManager fragmentManager = getFragmentManager();
         fragmentManager.beginTransaction().replace(R.id.content_frame, fragment).commit();
 
-        // update selected item and title, then close the drawer
-//        mDrawerList.setItemChecked(position, true);
-//        setTitle(mPlanetTitles[position]);
         mDrawerLayout.closeDrawer(mDrawerList);
 	}
 	
